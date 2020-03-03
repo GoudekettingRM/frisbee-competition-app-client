6c05d00087bdcf6c8f1bb670096c853344502128
GoudekettingRM <r.goudeketting@gmail.com>
2020-03-03 17:57:21 +0100

    remove logs

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -8,7 +8,7 @@ export function signUp(userData) {
   return async (dispatch, getState) => {
     try {
       const sessionData = await axios.post(`${baseUrl}/players`, userData);
-      console.log("Session data . data", sessionData.data);
+
       dispatch(setSessionAction(sessionData.data));
     } catch (error) {
       throw error;
@@ -20,7 +20,7 @@ export function login(loginData) {
   return async (dispatch, getState) => {
     try {
       const sessionData = await axios.post(`${baseUrl}/login`, loginData);
-      console.log("session details", sessionData);
+
       dispatch(setSessionAction(sessionData.data));
     } catch (error) {
       throw error;
