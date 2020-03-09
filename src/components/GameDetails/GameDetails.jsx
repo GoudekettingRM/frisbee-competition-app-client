478475d292fea69e86f2f97a12a9bf3322a6fac3
GoudekettingRM <r.goudeketting@gmail.com>
2020-03-09 16:37:11 +0100

    add game details

--- src/components/GameDetails/GameDetails.jsx
+++ src/components/GameDetails/GameDetails.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { getOneGame } from "../../store/game/actions";
+import { headerSpacing } from "../../styles";
 
 class GameDetails extends Component {
   componentDidMount = () => {
@@ -13,15 +19,65 @@ class GameDetails extends Component {
     }
   };
 
+  loadTeamGameDetails = (homeOrAway, teamData) => {
+    const teamScore =
+      homeOrAway === "home"
+        ? this.props.game.homeTeamScore
+        : this.props.game.awayTeamScore;
+    const teamReceivedSpiritScore =
+      homeOrAway === "home"
+        ? this.props.game.homeTeamReceivedSpiritScore
+        : this.props.game.awayTeamReceivedSpiritScore;
+
+    const { name } = teamData;
+    return (
+      <div>
+        <ExpansionPanel>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header">
+            <Typography component="h2" style={{ marginRight: "15px" }}>
+              {name}
+            </Typography>
+            <Typography
+              component="h2"
+              variant="body1"
+              color="textSecondary"
+              style={{ fontSize: "0.8rem", marginTop: "3px" }}>
+              <em>Received spirit: {teamReceivedSpiritScore}</em>
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Spirit score received: {teamReceivedSpiritScore}
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </div>
+    );
+  };
+
   render() {
-    return <div></div>;
+    console.log("render of game details", this.props);
+
+    if (!Object.keys(this.props.game).length) return <div>No data</div>;
+    return (
+      <div style={headerSpacing}>
+        {this.loadTeamGameDetails("home", this.props.homeTeam)}
+        <h2>{this.props.game.homeTeamScore}</h2>
+        <p>-</p>
+        <h2>{this.props.game.awayTeamScore}</h2>
+        {this.loadTeamGameDetails("away", this.props.awayTeam)}
+      </div>
+    );
   }
 }
 
 const mapStateToProps = state => ({
   game: state.game.data,
-  homeTeam: state.game.homeTeam,
-  awayTeam: state.game.awayTeam
+  homeTeam: state.game.data.homeTeam,
+  awayTeam: state.game.data.awayTeam
 });
 
 const mapDispatchToProps = { getOneGame };
