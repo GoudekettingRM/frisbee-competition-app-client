fe6631d60cde5b01c06ed75f2f443a010ca662a2
GoudekettingRM <r.goudeketting@gmail.com>
2020-03-14 21:32:11 +0100

    revert back from env variable

--- src/api.js
+++ src/api.js
@@ -1,4 +1,7 @@
-export const baseUrl = process.env.API_BASE_URL || "http://localhost:4000";
+export const baseUrl = "https://ulti-mate.herokuapp.com";
+
+// local: http://localhost:4000
+//https://ulti-mate.herokuapp.com
 
 export function authHeader(token, otherHeaders) {
   return {
